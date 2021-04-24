19b3e68a708c8612d4cc921e80ba1cb83f506b87
Aiueokashi <akkey2017@gmail.com>
2021-04-24 02:11:16 +0000

    Updates

---
+++ precache-manifest.ae04b019620d88440ed330033376771a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6125b1a38bfe679552ecf7b13faf81",
+    "url": "/ReactDash/index.html"
+  },
+  {
+    "revision": "68ac5c7820be97bc2dd3",
+    "url": "/ReactDash/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "92fa6b90c1d32186fa68",
+    "url": "/ReactDash/static/js/2.b9a68bad.chunk.js"
+  },
+  {
+    "revision": "68ac5c7820be97bc2dd3",
+    "url": "/ReactDash/static/js/main.46a4acea.chunk.js"
+  },
+  {
+    "revision": "409c136682f2f0ddc3dc",
+    "url": "/ReactDash/static/js/runtime-main.5ebf1c33.js"
+  }
+]);
